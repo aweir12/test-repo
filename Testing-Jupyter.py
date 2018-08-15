a44ae558b7ad9f406a246318552f5317f1ac1858
aweir12 <austin.weir@gmail.com>
2018-08-15 15:56:44 +0000

    adding output

--- Testing-Jupyter.py
+++ Testing-Jupyter.py
@@ -1,15 +1,23 @@
 
 # coding: utf-8
 
-# In[1]:
-
-
-import cx_Oracle
-import params
-
-
 # In[2]:
 
 
-print(params.password)
+import cx_Oracle
+
+
+# In[3]:
+
+
+print("hello")
+
+
+# In[8]:
+
+
+i = 0
+while i < 100:
+    print(i)
+    i = i + 1
 
