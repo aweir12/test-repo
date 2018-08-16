a91766f795b6b11183b438c6905b9741c9781231
aweir12 <austin.weir@gmail.com>
2018-08-16 16:50:20 +0000

    test

--- Testing-Jupyter.py
+++ Testing-Jupyter.py
@@ -1,23 +1,29 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[ ]:
 
 
 import cx_Oracle
 
 
-# In[3]:
+# In[ ]:
 
 
 print("hello")
 
 
-# In[8]:
+# In[1]:
 
 
 i = 0
 while i < 100:
     print(i)
     i = i + 1
+
+
+# In[2]:
+
+
+print("test")
 
